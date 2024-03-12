5563a1c109f68757ee319c200762e5fbaaf98cef
Akshay <108143027+AkshayNS28@users.noreply.github.com>
2024-03-12 13:06:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://akshayns28.github.io/MyPortfolio/',
+  base: '/akshayns28.github.io/MyPortfolio/',
 })
